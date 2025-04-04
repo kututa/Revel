69738166b3b6de5a08d7949be89a6352539ee797
kututa <vinnykututa@gmail.com>
2025-04-04 14:04:43 +0300

    Fix casing for Homepage.jsx to work on Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './assets/components/Navbar';
-import HomePage from './assets/components/Homepage';  
+import HomePage from './assets/components/Homepage';  // Make sure the file is named "Homepage.jsx"
 import Footer from './assets/components/Footer';
 
 function App() {
   return (
     <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-          <footer>
-            <Footer />
-          </footer>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <footer>
+        <Footer />
+      </footer>
     </Router>
   );
 }
